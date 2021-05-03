ab1ac9700d0dd809a1ee2113f601239b5f03c26c
Stas <stas.krot1996@gmail.com>
2021-05-03 17:40:16 +0300

    Map ok

--- app/src/main/java/com/example/future_parking/activities/DataActivity.java
+++ app/src/main/java/com/example/future_parking/activities/DataActivity.java
@@ -19,7 +19,6 @@ public class DataActivity extends AppCompatActivity {
     private EditText data_EDT_email;
     private Button data_BTN_save;
     private Account account;
-    int n;
     String newName;
     String newEmail;
     String newPassword;
